7d591225acc81ff5bae356f0fb231ba6949a6a1c
Seungmin Shin <s091508@gmail.com>
2019-07-29 16:26:56 +0900

    Add files via upload

--- cansat_turnaround.ino
+++ cansat_turnaround.ino
@@ -1,10 +1,14 @@
 float previous_altitude=0,current_altitude=0;
 float distance,velocity_air,velocity_fall;
-
+float height,cansat_height,ground_height;
+float time_land;
 void updateturnaround(){
   distance=cansatLocation.distance;  
   velocity_air=cansatGPS.ground_speed_ms();
   previous_altitude=current_altitude;
   current_altitude=cansatGPS.location().alt/100.;
   velocity_fall=previous_altitude-current_altitude;
+  cansat_height=current_altitude;
+  height=cansat_height-ground_height;
+  
 }
